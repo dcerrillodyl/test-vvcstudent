c3e9c915ccc4a1a0c2a8ea03b9cbc71eacff6504
vvcstudent <0005076@student.vvc.edu>
2017-05-23 11:48:36 -0700

    Create Main.java
    Added a Main.java file

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+
+public class Main  {
+    public static void main(String args []){
+        Set<Point> pointSet1 = new HashSet<Point>();
+        pointSet1.add(new Point(0.0,0.0));
+        pointSet1.add(new Point(1.0,1.0));
+
+        for(Point p : pointSet1) {
+            System.out.println(p);
+        }
+    }
+}
